aff2fa4835ca5faf299e272b89ed22a12400148f
RajPK <pkrajanand@gmail.com>
2018-09-23 18:46:34 +1000

    formatted test

--- src/test/java/com/weatherforecast/service/impl/CoveredCityLoaderServiceTest.java
+++ src/test/java/com/weatherforecast/service/impl/CoveredCityLoaderServiceTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.weatherforecast.service.impl;
 
 import static org.junit.Assert.assertTrue;
@@ -13,24 +10,14 @@ import org.junit.Test;
 
 import com.weatherforecast.service.impl.CoveredCityLoaderService;
 
-/**
- 
- *
- */
 public class CoveredCityLoaderServiceTest {
 
 	private CoveredCityLoaderService coveredCityLoaderService = new CoveredCityLoaderService();
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@Before
 	public void setUp() throws Exception {
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@After
 	public void tearDown() throws Exception {
 	}
@@ -48,5 +35,4 @@ public class CoveredCityLoaderServiceTest {
 		assertTrue(coveredCities.get(1).equals("Melbourne"));
 
 	}
-
 }
