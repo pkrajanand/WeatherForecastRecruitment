5f44eaae9ecdc7df984e29b32de4a8bd1b34a694
RajPK <pkrajanand@gmail.com>
2018-09-24 10:45:18 +1000

    made url mapping at the method level

--- src/main/java/com/weatherforecast/web/LocationController.java
+++ src/main/java/com/weatherforecast/web/LocationController.java
@@ -5,22 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.weatherforecast.model.Location;
 import com.weatherforecast.service.LocationService;
 
 @Controller
-@RequestMapping("/locations")
 public class LocationController {
 
 	@Autowired
     @Qualifier(value = "flatFileLocationService")
 	private LocationService locationService;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping("/locations")
 	public @ResponseBody List<Location> getCoveredCities() {
 		return locationService.getLocations();
 	}
