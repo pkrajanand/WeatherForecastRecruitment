76e354d4d22e67723120ee38bb284d499669aef3
RajPK <pkrajanand@gmail.com>
2018-09-25 21:30:36 +1000

    Facilitate separate deployment of front-end and back-end #1; Introduced NodeJS Express to host front-end on a different endpoint

---
+++ front-end/public/scripts/weatherApp.js
@@ -0,0 +1,25 @@
+var app = angular.module('weatherForecastingApp', []);
+
+app.constant('config', {
+    protocol: 'http',
+    host: 'localhost',
+    port: 8080
+});
+
+app.controller('weatherForecastController', ['$scope', '$http', 'config', function($scope, $http, config) {
+    $http.get(config.protocol+"://"+config.host+":"+config.port+"/locations").success(function(response) {
+        $scope.data = {
+            citySelect: null,
+            availableOptions: response.map(location => location.name)
+        };
+    });
+    
+	$scope.getWeather = function() {
+	    $http.get(config.protocol+"://"+config.host+":"+config.port+"/weather/" + $scope.data.citySelect)
+	    .success(function(response) {
+	            $scope.weatherData = response;
+	    });
+	}
+}]);
+
+
