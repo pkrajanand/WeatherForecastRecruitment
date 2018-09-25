3c78f39f1d51d153462660d1445105a54ee2cb1d
RajPK <pkrajanand@gmail.com>
2018-09-25 21:31:28 +1000

    Facilitate separate deployment of front-end and back-end #1; Introduced CORS filter in backend to allow cross origin resource sharing with separate endpoint configuration for frontend and backend

--- src/main/java/com/weatherforecast/WeatherForecaster.java
+++ src/main/java/com/weatherforecast/WeatherForecaster.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Weather Forecaster (WeFor) built using Spring-Boot
- * 
  */
 @SpringBootApplication
 public class WeatherForecaster {
@@ -13,4 +12,4 @@ public class WeatherForecaster {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherForecaster.class, args);
 	}
-}
+	}
