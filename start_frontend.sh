4aa06d7a77cb0ac6d986ab9810ef22a362812cfe
RajPK <pkrajanand@gmail.com>
2018-09-26 22:31:15 +1000

    renamed frontend home folder

--- start_frontend.sh
+++ start_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker logout
-docker build -t pkrajanand/weatherui:1.0 ./front-end 
+docker build -t pkrajanand/weatherui:1.0 ./frontend 
 docker images | grep weatherui
-docker-compose -f ./front-end/docker-compose.yml up
+docker-compose -f ./frontend/docker-compose.yml up
